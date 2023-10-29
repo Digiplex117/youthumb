5127853a31e260d2295f336e7e563b26d4dda7e7
aymane <aymane.elaouni5@gmail.com>
2023-10-29 23:37:11 +0100

    adding page

--- pages/_document.js
+++ pages/_document.js
@@ -26,11 +26,29 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+          {/* Add your paragraph here */}
+          <p>
+            YouTube Thumbnail Downloader
+            ▷ Save YT Video Thumbnails
+            Get free thumbnail image of any youtube videos in Full HD(1080), HD
+            (720), SD, and also in small size. it's currently supported formats:
+            YouTube (HD, HQ, 1080p, 4K) videos
+          </p>
+          <p>
+            What is the use of this YouTube Thumbnail Grabber website?
+            People use this YouTube thumbnail downloader website for getting
+            thumbnail from any youtube videos. That can be used in presentation,
+            animation work or many other activities.
+          </p>
+          {/* ... and so on for the rest of the text */}
           <NextScript />
         </body>
       </Html>
     );
   }
 }
+
+Make sure to add each paragraph as a separate `<p>` element within the `<body>` section of your custom document. After saving the changes, you can restart your Next.js development server to see the added text on your website.
+
 
 export default MyDocument;
